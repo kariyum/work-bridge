134ac083f641031bd5f70d462e229b85f92355d3
Karim Ben Amara <karimbenamara91@gmail.com>
2024-11-18 21:55:56 +0100

    update how we handle promises

--- app/src/routes/messages/[id]/+page.svelte
+++ app/src/routes/messages/[id]/+page.svelte
@@ -2,6 +2,7 @@
 	import { userStore } from '$lib/storage.js';
 	import { onDestroy } from 'svelte';
 	import { tick } from 'svelte';
+	import { SendHorizontal } from 'lucide-svelte';
 
 	const { data } = $props();
 
@@ -116,55 +117,25 @@
 </div>
 <div class="input">
 	<form onsubmit={onClick} class="input-form">
-		<input type="text" bind:value={message} />
-		<input type="submit" value="Send" />
+		<input type="text" bind:value={message} placeholder="Type a message..."/>
+		<button type="submit">
+			<SendHorizontal class="icon"/>
+		</button>
+
 	</form>
 </div>
 
 <style>
-	.component {
-		width: 100%;
-		height: 80vh;
-		display: flex;
+	:global(.icon) {
+		background-color: transparent;
 	}
-
-	.container {
-		display: flex;
-		width: 90%;
-		margin: auto;
-		height: 100%;
-		gap: 1rem;
-	}
-
-	.discussions {
-		flex: 1;
-		padding: 1rem;
-		overflow-y: auto;
-		border: 1px solid rgb(204, 204, 204);
-		border-radius: 5px;
-	}
-
-	.messages-col {
-		flex: 3;
-		display: flex;
-		flex-direction: column;
-		background-color: #fff;
-	}
-
-	.header {
-		font-size: 1.5rem;
-		margin-bottom: 1rem;
-		color: #333;
-		width: min-content;
-		margin: auto;
-	}
-
 	.messages {
 		flex-grow: 1;
 		overflow-y: auto;
 		padding: 1rem;
 		border: 1px solid #ddd;
-		border-radius: 5px;
+		border-left: 0px;
+		/* border-radius: 5px; */
 		background-color: #f7f7f7;
 	}
 
@@ -190,7 +161,6 @@
 	.input {
 		display: flex;
 		width: 100%;
-		padding-top: 1rem;
 	}
 
 	.input-form {
@@ -202,13 +172,21 @@
 		flex: 1;
 		padding: 0.75rem;
 		border: 1px solid #ccc;
-		border-radius: 5px;
-		margin-right: 0.5rem;
+		border-radius: 0;
+		border: 0;
+		border-bottom: 1px solid #ccc;
+		border-right: 1px solid #ccc;
+		outline: none;
 	}
 
-	.input-form input[type='submit'] {
+	.input-form button[type='submit'] {
 		padding: 0.75rem 1rem;
-		border-radius: 5px;
 		cursor: pointer;
+		background-color: #fff;
+		border-left: 0;
+		border-top: 0;
+		border-bottom: 1px solid #ccc;
+		border-right: 1px solid #ccc;
+		border-radius: 0;
 	}
 </style>
