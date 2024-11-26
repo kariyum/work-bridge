545cdc3a828ddabd0f1cdd71fae4c2e79b13ef4a
Karim Ben Amara <karimbenamara91@gmail.com>
2024-11-26 12:17:56 +0100

    refactor function names

--- server/src/repository/project.rs
+++ server/src/repository/project.rs
@@ -1,9 +1,10 @@
 use chrono::{DateTime, Utc};
 use serde::Deserialize;
 use sqlx::{Executor, Postgres};
+use crate::tasks::repo::TaskCreate;
 
 #[derive(sqlx::FromRow)]
-struct RawProject {
+struct ProjectRaw {
     id: i32,
     user_id: String,
     title: String,
@@ -14,32 +15,33 @@ struct RawProject {
     created_at: DateTime<Utc>,
 }
 
-async fn get_project_by_id(project_id: i32, conn: impl Executor<'_, Database=Postgres>) -> Result<Option<RawProject>, sqlx::Error> {
-    let project = sqlx::query_as::<_, RawProject>("select * from projects where id = $1")
+async fn get_project_by_id(project_id: i32, conn: impl Executor<'_, Database=Postgres>) -> Result<Option<ProjectRaw>, sqlx::Error> {
+    let project = sqlx::query_as::<_, ProjectRaw>("select * from projects where id = $1")
         .bind(project_id)
         .fetch_optional(conn)
         .await;
     project
 }
 
-async fn get_projects(conn: impl Executor<'_, Database=Postgres>) -> Result<Vec<RawProject>, sqlx::Error> {
-    let projects = sqlx::query_as::<_, RawProject>("select * from projects")
+async fn get_projects(conn: impl Executor<'_, Database=Postgres>) -> Result<Vec<ProjectRaw>, sqlx::Error> {
+    let projects = sqlx::query_as::<_, ProjectRaw>("select * from projects")
         .fetch_all(conn)
         .await;
     projects
 }
 
 #[derive(Deserialize)]
-pub struct CreateProject {
+pub struct ProjectCreate {
     user_id: String,
     title: String,
     content: String,
     deadline: DateTime<Utc>,
     budget: f32,
     currency_code: String,
+    tasks: Vec<TaskCreate>
 }
 
-async fn project_create(create_project: CreateProject, conn: impl Executor<'_, Database=Postgres>) -> Result<(), sqlx::Error> {
+async fn insert_project(create_project: ProjectCreate, conn: impl Executor<'_, Database=Postgres>) -> Result<(), sqlx::Error> {
     sqlx::query("
         INSERT INTO projects (user_id, title, content, deadline, budget, currency_code)
         VALUES ($1, $2, $3, $4, $5, $6)
@@ -73,16 +75,17 @@ mod tests {
 
     #[sqlx::test(fixtures(path = "./fixtures", scripts("projects.sql")))]
     async fn create_project_test(conn: PgPool) {
-        let project_details = CreateProject {
+        let project_details = ProjectCreate {
             user_id: "user@gmail.com".to_string(),
             title: "title".to_string(),
             content: "content".to_string(),
             deadline: Utc::now(),
             budget: 10.5,
             currency_code: "TD".to_string(),
+            tasks: vec!()
         };
 
-        project_create(project_details, &conn)
+        insert_project(project_details, &conn)
             .await
             .expect("Failed to create project");
     }
