1e46e483f46981e6c6d7894cc46967c46c8a2ca6
Karim Ben Amara <karimbenamara91@gmail.com>
2024-12-18 22:47:23 +0100

    Update Skills component
    Add profile page

--- app/src/routes/profile/+page.svelte
+++ app/src/routes/profile/+page.svelte
@@ -1,15 +1,47 @@
 <script lang="ts">
+	import Skills from "$lib/components/Skills.svelte";
+	import { TaskClass } from "$lib/states.svelte";
+
     let { data } = $props();
+    let skills: string[] = $state([]);
+    const addSkill = (skill: string) => skills.push(skill);
+    const removeSkillAtIndex = (index: number) => skills.splice(index, 1);
 </script>
 
 <div class="container">
-    <h1>You are {data.user?.email}</h1>
+    <h2>Welcome {data.user?.email}</h2>
+    
+    <p>Edit your profile</p>
+    <form>
+        <label for="whoisit">Tell us about yourself</label>
+        <textarea id="whoisit" placeholder="Tell us about yourself" value={""}></textarea>
+        <label for="first_name">First Name</label>
+        <input type="text" placeholder="First name" value={""} />
+        <label for="last_name">Last Name</label>
+        <input type="text" placeholder="Last name" value={""} />
+        <label for="skills">Skills</label>
+        <div class="skills">
+            <Skills skillsIn={skills} {addSkill} {removeSkillAtIndex}  ></Skills>
+        </div>
+        <button>Save</button>
+    </form>
 </div>
 
 <style>
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    .skills {
+        border: 1px solid #ccc;
+        padding: 0.5rem;
+        border-radius: 5px;
+    }
+    label {
+        width: fit-content;
+        margin-left: auto;
+    }
+    form {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        max-width: 500px;
+        margin: auto;
+        gap: 0.5rem 1rem;
     }
 </style>
