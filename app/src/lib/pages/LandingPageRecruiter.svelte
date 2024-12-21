13de89b0dcb09a1e84411f9dc9cfbf5152d7b001
Karim Ben Amara <karimbenamara91@gmail.com>
2024-12-21 21:42:13 +0100

    Update recruiter Page styling

--- app/src/lib/pages/LandingPageRecruiter.svelte
+++ app/src/lib/pages/LandingPageRecruiter.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import Project from '$lib/components/Project.svelte';
+	import Project from '$lib/client/components/Project.svelte';
 	import type { ProjectGET } from '$lib/types/project';
 	let { projects }: { projects: ProjectGET[] } = $props();
 </script>
 
 <div class="outer-container">
+	<h2>My Projects</h2>
 	<div class="container">
-		<h1>My Projects</h1>
 		{#if projects.length === 0}
 			<p>No projects found, please create one!</p>
 		{:else}
@@ -22,6 +22,9 @@
 		margin: auto;
 	}
 	.container {
-		margin: 1rem 0rem;
+		margin-top: 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 	}
 </style>
