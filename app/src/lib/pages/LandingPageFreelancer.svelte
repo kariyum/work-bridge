bbf96332d9bc2b76e7e9dad3418a08bf1ed3f174
Karim Ben Amara <karimbenamara91@gmail.com>
2025-02-04 23:19:18 +0100

    Add freelancer Project component
    Add arabic currency icon

--- app/src/lib/pages/LandingPageFreelancer.svelte
+++ app/src/lib/pages/LandingPageFreelancer.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
-	import ProjectFreelancer from "$lib/components/ProjectFreelancer.svelte";
-	import type { ProjectGET } from "$lib/types/project";
-    let { projects }: {projects: ProjectGET[]} = $props();
+	import Project from '$lib/freelancer/components/Project.svelte';
+	import type { ProjectGET } from '$lib/types/project';
+	let { projects }: { projects: ProjectGET[] } = $props();
 </script>
 
 <div class="outer-container">
-    <h2>Projects Feed</h2>
-    {#each projects as project}
-        <ProjectFreelancer {project} />
-    {/each}
+	<h3>My Projects</h3>
+	<div>
+		{#if projects.length === 0}
+			<p>Sorry for the inconvenience, no projects were found.</p>
+		{:else}
+			<div class="container">
+				{#each projects as project}
+					<Project {project} />
+				{/each}
+			</div>
+		{/if}
+	</div>
 </div>
 
-
 <style>
-
+	.outer-container {
+		margin: auto;
+	}
+	.container {
+		margin-top: 1rem;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		gap: 1rem;
+	}
 </style>
