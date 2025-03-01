ea6e86447464bddef3f34e1493e22323d627a537
Karim Ben Amara <karimbenamara91@gmail.com>
2025-03-01 15:51:47 +0100

    Delete redundant /project/task page

--- app/src/routes/project/task/+page.ts
+++
@@ -1,7 +0,0 @@
-export async function load({ parent, url }) {
-    const index = url.searchParams.get('s')
-    const result = { selectedIndex: index ? parseInt(index) : undefined };
-    // const parentData = await parent();
-    // parentData.tasksGlobalState.selectTask(result.selectedIndex ?? -1);
-    return result;
-}
