ad16215b2a862b106cb50e5ecf350dbb78fe2c75
Karim Ben Amara <karimbenamara91@gmail.com>
2025-03-04 23:25:18 +0100

    Migrate proposals_handler to user auth extractor

--- server/src/routes/proposals_handler.rs
+++ server/src/routes/proposals_handler.rs
@@ -1,8 +1,8 @@
 use crate::repository::project::ProjectRaw;
-use crate::services::token::validate_jwt;
+use crate::services::token::Claims;
 use actix_web::dev::HttpServiceFactory;
 use actix_web::web::{Json, Path};
-use actix_web::{web, HttpRequest, HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 
@@ -10,7 +10,7 @@ use sqlx::PgPool;
 struct ProposalRow {
     id: i32,
     user_id: String,
-    project_id: i32,
+    task_id: i32,
     status: i32,
     budget: Option<f32>,
     content: Option<String>,
@@ -19,84 +19,69 @@ struct ProposalRow {
 
 #[derive(Deserialize)]
 struct ProposalCreate {
-    project_id: i32,
+    task_id: i32,
     status: Option<i32>,
     budget: Option<f32>,
     content: Option<String>,
 }
 
 pub async fn create_proposal(
-    request: HttpRequest,
+    claims: Claims,
     proposal_create: Json<ProposalCreate>,
     pgpool: web::Data<PgPool>,
 ) -> impl Responder {
-    let cookie = request
-        .cookie("Authorization")
-        .map(|token| validate_jwt(token.value()).ok())
-        .flatten();
+    let mut client = pgpool
+        .acquire()
+        .await
+        .expect("Failed to acquire a Postgres connection from the pool");
 
-    if let Some(claims) = cookie {
-        let mut client = pgpool
-            .acquire()
-            .await
-            .expect("Failed to acquire a Postgres connection from the pool");
-
-        let proposals = sqlx::query_as::<_, ProposalRow>(
-            "INSERT INTO proposals (
+    let proposals = sqlx::query_as::<_, ProposalRow>(
+        "INSERT INTO proposals (
                 user_id,
-                project_id,
+                task_id,
                 status,
                 budget,
                 content
             ) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-        )
-        .bind(&claims.sub)
-        .bind(&proposal_create.project_id)
-        .bind(0)
-        .bind(&proposal_create.budget)
-        .bind(&proposal_create.content)
+    )
+    .bind(&claims.sub)
+    .bind(&proposal_create.task_id)
+    .bind(0)
+    .bind(&proposal_create.budget)
+    .bind(&proposal_create.content)
+    .fetch_optional(&mut *client)
+    .await
+    .expect("Failed to insert proposal into database");
+
+    let project = sqlx::query_as::<_, ProjectRaw>("SELECT * FROM projects WHERE id = $1")
+        .bind(&proposal_create.task_id)
         .fetch_optional(&mut *client)
         .await
-        .expect("Failed to insert proposal into database");
-        let project = sqlx::query_as::<_, ProjectRaw>("SELECT * FROM projects WHERE id = $1")
-            .bind(&proposal_create.project_id)
+        .expect("Failed to fetch project from database");
+
+    if let Some(project) = project {
+        let exists = sqlx::query("SELECT * FROM discussions where user_ids = $1")
+            .bind(vec![&claims.sub, &project.user_id])
             .fetch_optional(&mut *client)
             .await
-            .expect("Failed to fetch project from database");
-        if let Some(project) = project {
-            let exists = sqlx::query("SELECT * FROM discussions where user_ids = $1")
-                .bind(vec![&claims.sub, &project.user_id])
-                .fetch_optional(&mut *client)
-                .await
-                .expect("Failed to fetch discussion from database")
-                .is_some();
-            if !exists {
-                sqlx::query("INSERT INTO discussions (user_ids, created_by, created_at) VALUES ($1, $2, $3) RETURNING *")
-                    .bind(vec![&claims.sub, &project.user_id])
-                    .bind(&claims.sub)
-                    .bind(chrono::Utc::now())
-                    .execute(&mut *client)
-                    .await
-                    .expect("Failed to insert discussion into database");
-            }
-        }
+            .expect("Failed to fetch discussion from database")
+            .is_some();
 
-        HttpResponse::Created().json(proposals)
-    } else {
-        HttpResponse::Unauthorized().finish()
+        if !exists {
+            sqlx::query("INSERT INTO discussions (user_ids, created_by, created_at) VALUES ($1, $2, $3) RETURNING *")
+                .bind(vec![&claims.sub, &project.user_id])
+                .bind(&claims.sub)
+                .bind(chrono::Utc::now())
+                .execute(&mut *client)
+                .await
+                .expect("Failed to insert discussion into database");
+        }
     }
+
+    HttpResponse::Created().json(proposals)
 }
 
-pub async fn get_proposals(request: HttpRequest, pgpool: web::Data<PgPool>) -> impl Responder {
-    let cookie = request
-        .cookie("Authorization")
-        .map(|token| validate_jwt(token.value()).ok())
-        .flatten();
-
-    if cookie.is_none() {
-        return HttpResponse::Unauthorized().finish();
-    }
-
+pub async fn get_proposals(_: Claims, pgpool: web::Data<PgPool>) -> impl Responder {
     let mut client = pgpool
         .acquire()
         .await
@@ -111,48 +96,26 @@ pub async fn get_proposals(request: HttpRequest, pgpool: web::Data<PgPool>) -> i
 }
 
 pub async fn delete_proposal(
-    request: HttpRequest,
+    _: Claims,
     path: Path<i32>,
     pgpool: web::Data<PgPool>,
 ) -> impl Responder {
-    let cookie = request
-        .cookie("Authorization")
-        .map(|token| validate_jwt(token.value()).ok())
-        .flatten();
-
-    if cookie.is_none() {
-        return HttpResponse::Unauthorized().finish();
-    }
-
     let mut client = pgpool
         .acquire()
         .await
         .expect("Failed to acquire a Postgres connection from the pool");
 
-    let proposal_id = path.into_inner();
+    let task_id = path.into_inner();
     let _ = sqlx::query("DELETE FROM proposals WHERE id = $1")
-        .bind(&proposal_id)
+        .bind(&task_id)
         .execute(&mut *client)
         .await
-        .expect(format!("Failed to delete project from the database {proposal_id}").as_str());
+        .expect(format!("Failed to delete proposal from the database {task_id}").as_str());
 
     HttpResponse::Ok().finish()
 }
 
-pub async fn get_proposal(
-    request: HttpRequest,
-    path: Path<i32>,
-    pgpool: web::Data<PgPool>,
-) -> impl Responder {
-    let cookie = request
-        .cookie("Authorization")
-        .map(|token| validate_jwt(token.value()).ok())
-        .flatten();
-
-    if cookie.is_none() {
-        return HttpResponse::Unauthorized().finish();
-    }
-
+pub async fn get_proposal(_: Claims, path: Path<i32>, pgpool: web::Data<PgPool>) -> impl Responder {
     let mut client = pgpool
         .acquire()
         .await
