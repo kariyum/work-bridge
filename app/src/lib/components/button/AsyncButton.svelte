d355bc4610a7307969fd548f1c316d5163fdcd40
Karim Ben Amara <karimbenamara91@gmail.com>
2025-05-17 22:13:23 +0100

    Add async button for api calls

---
+++ app/src/lib/components/button/AsyncButton.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let waitingForResponse = $state(false);
+	let isLoading = $state(false);
+	let disabled = $derived(waitingForResponse);
+	let done = $state(false);
+
+	interface Props {
+		onclick: (event: MouseEvent) => Promise<any>;
+		idleView: Snippet;
+		endView: Snippet;
+	}
+
+	let { onclick, idleView, endView }: Props = $props();
+
+	async function clickHandler(event: MouseEvent) {
+		waitingForResponse = true;
+		const spinnerTimeout = setTimeout(() => (isLoading = true), 200);
+		await onclick(event);
+		clearTimeout(spinnerTimeout);
+		waitingForResponse = false;
+		done = true;
+		setTimeout(() => (done = false), 1000);
+	}
+</script>
+
+<button {disabled} onclick={clickHandler}>
+	{#if isLoading}
+		<span class="loader"></span>
+	{:else if done}
+		{@render endView()}
+	{:else}
+		{@render idleView()}
+	{/if}
+</button>
+
+<style>
+	button {
+		width: 8rem;
+	}
+	.loader {
+		width: 1rem;
+		height: 1rem;
+		border: 5px dotted #fff;
+		display: inline-block;
+		position: relative;
+		box-sizing: border-box;
+		animation: rotation 2s linear infinite;
+	}
+
+	@keyframes rotation {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
